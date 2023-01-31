909e960799d5fed40fcb4c4efba8d01d3ae110a6
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-01-30 22:11:55 -0400

    created inquirer menu section

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
+const express = require('express');
+const db = require('./connection')
+
+const inquirer = require('inquirer');
+const table = require('console.table');
+
+// Question Section
+
+function chooseQuestion() {
+  inquirer.prompt(
+    {
+      type: 'list',
+      name: 'options',
+      message: 'What would you like to do?',
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update employee role",
+      ]
+    })
+    .then(response => {
+      switch (response.options) {
+        case "View all departments":
+          viewDepartments();
+          break;
+
+        case "View all roles":
+          viewRoles();
+          break;
+
+        case "View all employees":
+          viewEmployees();
+          break;
+
+        case "Add a department":
+          addDepartment();
+          break;
+
+        case "Add a role":
+          addRole();
+          break;
+
+        case "Add employee":
+          addEmployee();
+          break;
+
+        case "Update employee role":
+          updateRole();
+          break;
+      }
+    })
+};
+
 
 
