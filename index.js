91b254cc39a86f0e8169acf393c7fb064db55c8b
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-01-30 22:17:52 -0400

    created function to view employees

--- index.js
+++ index.js
@@ -73,4 +73,12 @@ function viewRoles() {
   })
 };
 
+function viewEmployees() {
+  db.query("SELECT employee.ID, employee.first_name, employee.last_name, role.title AS role_title, department.name AS dep_name, role.salary AS role_salary, CONCAT (manager.first_name, manager.last_name) AS manager FROM employee LEFT JOIN role ON role.ID = employee.role_id LEFT JOIN department ON department.ID = role.department_id LEFT JOIN  employee manager  ON employee.manager_id= manager.ID", (err, results) => {
+    if (err) throw err;
+    console.table(results)
+    chooseQuestion()
+  })
+};
+
 
